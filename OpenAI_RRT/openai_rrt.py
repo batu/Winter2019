6f4da80204f43fb44b8767cd0dfa03f0ea9d6017
batu <ba921@nyu.edu>
2019-01-17 11:45:24 -0800

    RRT working.

--- OpenAI_RRT/openai_rrt.py
+++ OpenAI_RRT/openai_rrt.py
@@ -7,17 +7,21 @@ from stable_baselines.common.cmd_util import make_atari_snapshot_env
 from stable_baselines.common.vec_env import SnapshotVecEnv
 from rrt_functions import no_embedding
 from rrt_functions import random_action
-
+from rrt_functions import euclidian_distance
+from rrt_functions import run_rrt
+from rrt_functions import get_random_goal
 
 # Set up variables
 ENV_NAME = 'PongNoFrameskip-v4'
-ENV_NAME = 'MountainCar-v0'
+# ENV_NAME = 'MountainCar-v0'
 seed = 0
 
 # Hyperparameters
-training_steps = 100000;
+training_steps = 10;
 embedding_function = no_embedding
 action_selection_function = random_action
+distance_measure = euclidian_distance
+get_goal = get_random_goal
 
 # Make the environment using StableBaselines.
 if "Frameskip" in ENV_NAME:
@@ -33,8 +37,6 @@ else:
     action_space = env.action_space
     env.environment_category = "Classic"
 
-
-
 print("\n########################################################################")
 print(f"#\n# RRT Starting on {ENV_NAME}")
 print(f"# The action space is: {action_space}")
@@ -42,26 +44,14 @@ print(f"# The observation space is:{observation_space}")
 print("#" )
 print(f"# The embedding function is {embedding_function.__name__}")
 print(f"# The action policy is {action_selection_function.__name__}")
+print(f"# The distance metric is {distance_measure.__name__}")
 print("# ")
 print("########################################################################\n")
 
+
 obs = env.reset()
-print("Initial state: ", env.envs[0].unwrapped.state)
 def main():
-    for _ in range(50):
-        obs, rewards, dones, info = env.step([0])
-        env.render()
-    saved_state = env.get_env_pickle()
-    print("Saved state: ", env.envs[0].unwrapped.state)
-    for _ in range(50):
-        obs, rewards, dones, info = env.step([1])
-        env.render()
-    print("Right before loading state: ", env.envs[0].unwrapped.state)
-    env.load_env_pickle(saved_state)
-    print("Loaded state: ", env.envs[0].unwrapped.state)
-    env.render()
-    while True:
-        pass
+    run_rrt(env, embedding_function, action_selection_function, distance_measure, get_goal)
 
 if __name__== "__main__":
     main()
--- OpenAI_RRT/rrt_functions.py
+++ OpenAI_RRT/rrt_functions.py
@@ -1,11 +1,71 @@
 import random
+import numpy as np
 
+
+def run_rrt(env, embedding_function, action_policy, distance_measure, get_goal, action_steps=8, max_steps = 10000, rendering=True, state_pruning=True):
+
+    unwrapped_env = env.envs[0].unwrapped
+    observation_space = env.reset().shape
+    action_space = unwrapped_env.action_space
+
+    # assert type(env) == SnapshotVecEnv
+    obs = env.reset()
+
+    observations = [obs]
+    embeddings = [embedding_function(obs)]
+    pickles = [env.get_env_pickle()]
+
+    for i in range(max_steps):
+        goal = get_goal(obs)
+        chosen_index = np.argmin([distance_measure(em_obs, goal) for em_obs in embeddings])
+        #print(chosen_index)
+
+        chosen_pickle = pickles[chosen_index]
+        env.load_env_pickle(chosen_pickle)
+
+        chosen_observation = observations[chosen_index]
+        chosen_action = [action_policy(chosen_observation, action_space)]
+
+        for _ in range(action_steps):
+            obs, rewards, dones, info = env.step(chosen_action)
+            if(any(dones)):
+                obs = env.reset()
+                print("Env reset!");
+
+        if(rendering and i % 50 == 0):
+            env.render()
+
+        if state_pruning:
+            test_arr = np.asarray(observations)
+            prune = np.all(np.isin(obs[0], test_arr))
+            if not prune:
+                observations.append(obs)
+                embeddings.append(embedding_function(obs))
+                pickles.append(env.get_env_pickle())
+        else:
+            observations.append(obs)
+            embeddings.append(embedding_function(obs))
+            pickles.append(env.get_env_pickle())
+    env.unwrapped.close()
 
 # Embedding Functions
 def no_embedding(input: "numpy_array") -> "numpy_array":
     return input
 
+def GO_explore_downsampling(input: "numpy_array") -> "numpy_array":
+    return
+
 
 # Action selection functions
-def random_action(action_space: int) -> int:
-    return random.randint(0, action_space.n)
+def random_action(obs: "numpy_array", action_space: int) -> int:
+    return random.randint(0, action_space.n -1)
+
+
+# Distance Measures
+def euclidian_distance(pointA: "numpy_array", pointB: "numpy_array", _norm=np.linalg.norm) -> float:
+    return _norm(pointA - pointB)
+
+
+# Get Goal Functions
+def get_random_goal(obs):
+    return np.random.rand(*obs.shape) * 2 - 1
