c24922ade7afbf7e9ad0b29a942438ee8817c918
batu <ba921@nyu.edu>
2019-02-06 15:00:03 -0800

    Added the ability to save, manually and automatically the states from a saveplay.

--- Retro/movie_playback.py
+++ Retro/movie_playback.py
@@ -5,32 +5,50 @@ movie = retro.Movie('Movies/play_session_1.bk2')
 movie.step()
 
 game_name = "SuperMarioWorld-Snes"
-state_save_name = "smw_two_level_states"
+state_save_name = "every_100_from_level_1"
 env = retro.make(game=game_name, state=None, use_restricted_actions=retro.Actions.ALL, players=movie.players)
 env.initial_state = movie.get_state()
 env.reset()
-
-manual = True
 
 
 print("1 = 1, 2 = 10, 3 = 100, 4 = 1000, frames forward.")
 
 states = []
-frames_forward = 1
+total_frames = 0
 while True:
-    if manual:
-        try:
-            button = float(input('Enter your input: '))
-        except:
-            continue
-        if button == 9:
-            break
-        frames_forward = int(10 ** (button - 1))
-        print(frames_forward)
-    if frames_forward < 1:
+    frames_forward = 0
+    inp = input('Enter your input: ')
+    try:
+        number_input = float(inp)
+        frames_forward = int(10 ** (number_input - 1))
+        total_frames += frames_forward
+        print(f"Forwarding {frames_forward} frames, going to frame: {total_frames}.")
+    except:
+        pass
+    if inp == "q":
+        break
+    if inp == "s":
         states.append(env.em.get_state())
         print("State appended!")
-        frames_forward = 0
+    if inp == "r":
+        save_counter = int(input("Once how many frames should I save?"))
+        counter = save_counter
+        while movie.step():
+            keys = []
+            if counter == save_counter:
+                states.append(env.em.get_state())
+                print(f"Saved the {len(states)}th state at frame {total_frames}.")
+                counter = 0
+            for p in range(movie.players):
+                for i in range(env.num_buttons):
+                    keys.append(movie.get_key(i, p))
+            _obs, _rew, _done, _info = env.step(keys)
+            total_frames += 1
+            env.render()
+            counter += 1
+        else:
+            print("Done saving.")
+            break
     for _ in range(frames_forward):
         movie.step()
         keys = []
